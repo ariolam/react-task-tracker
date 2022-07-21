9a7bc57b965ed1c3363d4cce1fca904c2045132e
ariola <ariola.markou@gmail.com>
2022-07-21 12:04:01 +0300

    Created the structure and added styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
+import Tasks from "./Tasks.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> Hello from React</header>
-      <button className="btn btn-primary"> hello </button>
+      <div className="container">
+        <div className="task-overview">
+          <h1> Today is a wonderful day </h1>
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="text"
+                  placeholder="Enter a new task"
+                  autoFocus="on"
+                  className="form-control mt-3"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="button"
+                  value="Add"
+                  class="btn btn-outline-warning mt-3 text-white"
+                />
+              </div>
+            </div>
+          </form>
+          <Tasks />
+        </div>
+      </div>
     </div>
   );
 }
